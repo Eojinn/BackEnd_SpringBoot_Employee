2c257c68e5b7cad948e0a0978bc6e27a776eb6a0
Eojinn <aj41212@gmail.com>
2026-03-16 16:22:28 +0900

    update

--- Employee-Api/src/main/java/com/employee/api/repository/DepartmentRepository.java
+++
@@ -1,8 +0,0 @@
-package com.employee.api.repository;
-
-import com.employee.api.entity.Department;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DepartmentRepository extends JpaRepository<Department, Long> {
-
-}
